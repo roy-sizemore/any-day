257952b1c1eabd4ea9e9459b55c1280230255cf0
Roy Sizemore <rsizemore@swampcastle.home>
2021-02-24 21:40:08 -0500

    Updated script.js

--- script.js
+++ script.js
@@ -6,14 +6,13 @@ $('#currentDay').append(currentDate);
 const hoursArr = ['9 AM', '10 AM', '11 AM', '12 PM', '1 PM', '2 PM', '3 PM', '4 PM', '5 PM'];
 
 for (i = 0; i < hoursArr.length; i++) {
-    hrDiv = $('<div>').attr('id', 'hr' + hoursArr[i]).addClass('hour').text(hoursArr[i]);
+    // Use flex bootstrap to fix these divs!
+    let hrDiv = $('<div>').addClass('hour description').text(hoursArr[i]);
     textDiv = $('<div>').addClass('row textarea description');
     saveDiv = $('<div>').addClass('time-block saveBtn');
-    $('.container').append(hrDiv);
-    hrDiv.append(textDiv);
-    textDiv.append(saveDiv);
-    
-    let currentHour = moment().format('HH A');
+    $('.container').append(hrDiv, textDiv, saveDiv);
+
+    // let currentHour = moment().format('HH A');
     
     // if (currentHour.isBefore(hoursArr[i])) {
     //     hrDiv.addClass('past');

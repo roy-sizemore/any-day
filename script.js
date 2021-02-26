38eda9229adcbe6eb15490282af563e91560e7e9
Roy Sizemore <rsizemore@swampcastle.home>
2021-02-26 17:56:42 -0500

    Added past/present/future conditions; added click function to save button; added styling to save button

--- script.js
+++ script.js
@@ -3,28 +3,36 @@ let currentDate = moment().format('dddd, MMMM Do');
 $('#currentDay').append(currentDate);
 
 // Create a div, add row and hour classes, add hours to each row and append to .container
-const hoursArr = ['9AM', '10AM', '11AM', '12PM', '1PM', '2PM', '3PM', '4PM', '5PM'];
+const hoursArr = ['9 AM', '10 AM', '11 AM', '12 PM', '1 PM', '2 PM', '3 PM', '4 PM', '5 PM'];
 
-for (i = 0; i < hoursArr.length; i++) {
+for (i = 0; i < hoursArr.length; i++) {    
     const rowDiv = $('<div>').addClass('row time-block');
-    const textArea = $('<textarea>').addClass('description');
-    const hrDiv = $('<div>').addClass('hour col-1').text(hoursArr[i]);
-    const textDiv = $('<div>').addClass('row col-10');
-    const saveDiv = $('<div>').addClass('saveBtn col-1').append('<img src="./img/save.png">');
-    textDiv.append(textArea);
-    rowDiv.append(hrDiv, textDiv, saveDiv);
+    const textArea = $('<textarea>').attr('id', 'text' + i).addClass('col-10');
+    const hrDiv = $('<div>').addClass('hour col-1 pt-3').text(hoursArr[i]);
+    const saveDiv = $('<div>').attr('id', i).addClass('saveBtn col-1 img-fluid d-flex justify-content-center align-items-center').append('<img src="./img/save.png">');
+    rowDiv.append(hrDiv, textArea, saveDiv);
     $('.container').append(rowDiv);
 
-    // let currentHour = moment().format('HH A');
-    
-    // if (currentHour.isBefore(hoursArr[i])) {
-    //     hrDiv.addClass('past');
-    // } else if (currentHour.isSame(hoursArr[i]) ) {
-    //     hrDiv.addClass('present');
-    // } else {
-    //     hrDiv.addClass('future');
-    // };
+    let persist = localStorage.getItem(i);
+    textArea.val(persist);
 
-    function saveText() {};
+    let currentHour = moment(moment(), 'h A');
+    let hourCompare = moment(hoursArr[i], 'h A');
+    
+    if (moment().format('h A') === moment(hourCompare).format('h A')) {
+        textArea.addClass('present');
+    } else if (currentHour.isBefore(hourCompare)) {
+        textArea.addClass('future');
+    } else {
+        textArea.addClass('past');
+    };
+
+
 };
+
+$('.saveBtn').click(function () {
+    let btnId = $(this).attr('id');
+    let task = $('#text' + btnId).val();
+    localStorage.setItem(btnId, task);
+});
 
